d46e15d6f8515937571a1afcf640030fd4b02844
fangeang <cawfang50@gmail.com>
2023-10-11 21:26:36 +0700

    create contoller that return hello world

--- src/main/java/th/ac/chs/reg/RegApplication.java
+++ src/main/java/th/ac/chs/reg/RegApplication.java
@@ -2,12 +2,18 @@ package th.ac.chs.reg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RegApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RegApplication.class, args);
 	}
-
+@GetMapping("/hello")
+	public String hello(){
+		return "Hello World";
+}
 }
