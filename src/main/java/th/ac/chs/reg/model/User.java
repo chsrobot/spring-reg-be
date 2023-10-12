0ab36275a5c5e2b73a10b8629ab8a7f3aeb20b3c
oakkharaphop-nxz-group <oakkharaphop.mak@nxz-group.com>
2023-10-13 00:46:20 +0700

    feat:  add email in users

--- src/main/java/th/ac/chs/reg/model/User.java
+++ src/main/java/th/ac/chs/reg/model/User.java
@@ -6,17 +6,34 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 @Data
 @Entity
-@Table(name = "asset_balance_state")
+@Table(name = "users")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    @Column(name = "username",unique = true)
     private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
     private String password;
-    private Long created_at;
-    private Long updated_at;
+    @Column(name = "created_at")
+    private Long createdAt;
+
+    @Column(name = "updated_at")
+    private Long updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now().getEpochSecond();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now().getEpochSecond();
+    }
 }
