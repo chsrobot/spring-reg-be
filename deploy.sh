48758a448563ec47598f8415e1bf464b1665f86c
oakkharaphop-nxz-group <oakkharaphop.mak@nxz-group.com>
2023-10-16 15:00:44 +0700

    feat: add deployment script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build the Spring Boot application
+./mvnw clean package
+
+# Copy the generated JAR file to the deployment directory
+cp target/reg-0.0.1-SNAPSHOT.jar /home/adminreg/deployment/reg2024-be
+
+# Restart the application
+systemctl restart reg2024-be.service
