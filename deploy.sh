a7820cb9f584c27807b15f935b4ac265546770b2
oakkharaphop-nxz-group <oakkharaphop.mak@nxz-group.com>
2023-10-18 00:33:32 +0700

    ci: update flow

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
 ./mvnw clean package -DskipTests
 
 # Copy the generated JAR file to the deployment directory
+cd /home/adminreg/deployment/reg2024-be
 docker compose down
+cd /home/adminreg/reg2024/reg-be
 cp target/reg-0.0.1-SNAPSHOT.jar /home/adminreg/deployment/reg2024-be
+cd /home/adminreg/deployment/reg2024-be
 docker compose up -d --build
