72deb71aeb94e92657e33e7c6d896d7275ada5dd
Tawan50 <iamtawan50@gmail.com>
2023-12-05 21:17:33 +0700

    add status

--- src/main/java/th/ac/chs/reg/repository/ParentsDataRepository.java
+++ src/main/java/th/ac/chs/reg/repository/ParentsDataRepository.java
@@ -3,6 +3,6 @@ package th.ac.chs.reg.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import th.ac.chs.reg.model.ParentsData;
 
-public interface ParentsDataReporitory extends JpaRepository<ParentsData, Long> {
+public interface ParentsDataRepository extends JpaRepository<ParentsData, Long> {
     ParentsData findByUsername(String username);
 }
