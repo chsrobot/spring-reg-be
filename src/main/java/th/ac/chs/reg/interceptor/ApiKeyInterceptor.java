5dcf9e278a407109b08861cc404b85f40be12600
Blur Sotong <oakkharaphopmakanat@gmail.com>
2024-01-12 18:49:51 +0700

    chore: hardcoding temp

--- src/main/java/th/ac/chs/reg/interceptor/ApiKeyInterceptor.java
+++ src/main/java/th/ac/chs/reg/interceptor/ApiKeyInterceptor.java
@@ -1,6 +1,5 @@
 package th.ac.chs.reg.interceptor;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,15 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ApiKeyInterceptor implements HandlerInterceptor {
 
     private static final String API_KEY_HEADER = "X-Api-Key";
+    private static final String EXPECTED_API_KEY = "tm4WyNwsd9H5uQ6RTexqaKBX7GjLvhJpECSzfA28YVDrZ3Fckg"; // Replace with your actual API key
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Dotenv dotenv = Dotenv.configure().load();
-
-        String expectedApiKey = dotenv.get("API_KEY");
         String apiKey = request.getHeader(API_KEY_HEADER);
 
-        if (apiKey == null || !apiKey.equals(expectedApiKey)) {
+        if (apiKey == null || !apiKey.equals(EXPECTED_API_KEY)) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
